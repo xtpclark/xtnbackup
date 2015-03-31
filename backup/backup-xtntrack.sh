4a01d9c42077ec85b85a4bcd387032b55381304f
Perry Clark <pclark@xtuple.com>
2015-03-31 18:31:42 +0000

    changed machid

--- backup/backup-xtntrack.sh
+++ backup/backup-xtntrack.sh
@@ -221,13 +221,19 @@ DBSIZE=`ls -lh ${ARCHIVEDIR}/${BACKUPFILE} | cut -d' ' -f5`
 
 updatextnbu()
 {
+if [[ -z $(which ec2metadata) ]]; then
+MACHID=`hostname -f`
+else
+MACHID=`ec2metadata --instance-id`
+fi
 
 curl -X POST \
--d '{"CRMACCT":"'"${CRMACCT}"'", "STORAGEID":2, "CRMACCT_ID":"'"${CRMID}"'", "PGHOST":"'"${PGHOST}"'", "PGPORT":"'"${PGPORT}"'", "PGDB":"'"${PGDB}"'", "BACKUPFILE":"'"${BACKUPFILE}"'", "GLOBALFILE":"'"${GLOBALFILE}"'", "PGVER":"'"${PGDUMPVER}"'", "STARTJOB":"'"${STARTDBJOB}"'", "STOPJOB":"'"${STOPDBJOB}"'", "STARTRS":"'"${STARTRSJOB}"'", "STOPRS":"'"${STOPRSJOB}"'", "DBSIZE":"'"${DBSIZE}"'", "STOREURL":"'${BACKUPACCT}/${BACKUPFILE}'", "WASSPLIT":"'"${WASSPLIT}"'"}' \
+-d '{"CRMACCT":"'"${CRMACCT}"'", "STORAGEID":2, "CRMACCT_ID":"'"${CRMID}"'", "PGHOST":"'"${PGHOST}"'", "PGPORT":"'"${PGPORT}"'", "PGDB":"'"${PGDB}"'", "BACKUPFILE":"'"${BACKUPFILE}"'","GLOBALFILE":"'"${GLOBALFILE}"'", "PGVER":"'"${PGDUMPVER}"'", "STARTJOB":"'"${STARTDBJOB}"'", "STOPJOB":"'"${STOPDBJOB}"'", "STARTRS":"'"${STARTRSJOB}"'", "STOPRS":"'"${STOPRSJOB}"'","DBSIZE":"'"${DBSIZE}"'", "STOREURL":"'${BACKUPACCT}/${BACKUPFILE}'", "WASSPLIT":"'"${WASSPLIT}"'", "MACHID":"'"${MACHID}"'"}' \
 http://xtntrack.xtuple.com/ \
 --header "Content-Type:application/json"
 
 }
+
 
 
 mailcustreport()
